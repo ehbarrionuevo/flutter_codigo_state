d8235dc83f3f1f1bc22991924f8fcc86c429e06a
Elvis Barrionuevo <ehbarrionuevo@gmail.com>
2022-08-19 12:19:14 -0500

    Cambio del titulo 2

--- lib/main.dart
+++ lib/main.dart
@@ -12,7 +12,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: "StateApp Provider",
+      title: "StateApp Provider x",
       debugShowCheckedModeBanner: false,
       home: HomePage(),
     );
